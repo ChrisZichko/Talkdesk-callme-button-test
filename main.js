b174d77235f7fb77598bb4c1b55ad17e1fe6fcb4
Chris Zichko <tzichko@frontlineinsurance.com>
2022-07-18 16:56:49 -0400

    nothing much changing except a shell function.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// Need to add JS to connect to Talkdesk and trigger an agent
+// Need to add JS to connect to Talkdesk and trigger an agent in queue
+// When a user clicks "Call Me" then execute Callback API to Talkdesk with name and phone number.
+// This will put them into a "callback queue" that will allow an agent to call the user/customer. 
+const callMeBtn = (phoneNumber) => {
+
+}
